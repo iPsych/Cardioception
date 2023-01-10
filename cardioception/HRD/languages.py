0e8b285e047b05df8a0cda7dd02c3d2190a09eaf
iPsych <i.psych@icloud.com>
2023-01-10 10:21:01 +0900

    Update languages.py
    Korean Language Implementation.

--- cardioception/HRD/languages.py
+++ cardioception/HRD/languages.py
@@ -1,4 +1,8 @@
+#-*- coding:utf-8 -*-
+
 # Author: Nicolas Legrand <nicolas.legrand@cfin.au.dk>
+# Korean version : June Kang <june.kang@empathy-research.org>
+
 from typing import Collection, Dict
 
 
@@ -481,5 +485,126 @@ Comme la difficulté de la tâche évolue avec le temps, il est rare que vous so
         "Tutorial6"
     ] = """Ceci conclut le tutoriel. Si vous avez des questions, veuillez les poser maintenant à l'expérimentateur.
 Sinon, vous pouvez continuer avec la tâche principale."""
+
+    return texts
+
+def korean(device: str, setup: str, exteroception: bool) -> Dict[str, Collection[str]]:
+    """Create the text dictionary with instruction in Korean
+
+    Parameters
+    ----------
+    device : str
+        Can be `"keyboard"` or `"mouse"`.
+    setup : str
+        The experimental setup. Can be `"behavioral"` or `"test"`.
+    exteroception : bool
+        If `True`, the task includes and exteroceptive control condition.
+
+    Returns
+    -------
+    texts : dict
+
+    """
+    btnext = "스페이스를 누르세요" if device == "keyboard" else "마우스를 클릭하세요"
+    texts = {
+        "done": "과제를 완료했습니다. 참여해 주셔서 감사합니다.",
+        "slower": "느리게",
+        "faster": "빠르게",
+        "checkOximeter": "센서가 손가락에 올바르게 끼워졌는지 확인해 주십시오.",
+        "stayStill": "기록 중에는 가만히 있어 주십시오.",
+        "tooLate": "너무 늦었습니다.",
+        "correctResponse": "맞음",
+        "incorrectResponse": "틀림",
+        "VASlabels": ["추측", "확실"],
+        "textHeartListening": "당신의 심장에 귀기울여 보십시오.",
+        "textToneListening": "소리에 귀기울여 보십시오.",
+        "textTaskStart": "이제 과제가 시작됩니다. 준비하십시오.",
+        "textBreaks": f"쉬는 시간입니다. 원하는 만큼 쉴 수 있습니다. 과제를 다시 시작하려면 {btnext}.",
+        "textNext": f"계속하려면 {btnext}",
+        "textWaitTrigger": "fMRI 트리거를 기다리는 중 ...",
+        "Decision": {
+            "Intero": """삐 소리들이 당신의 심장보다 빠릅니까? 아니면 느립니까?""",
+            "Extero": """삐 소리들이 이전 것보다 빠릅니까? 아니면 느립니까?""",
+        },
+        "Confidence": """당신의 선택에 얼마나 자신이 있습니까?""",
+    }
+
+    if device == "keyboard":
+        texts["responseText"] = "느리게 하려면 아래 방향 키를 사용하고, 빠르게 하려면 위 방향 키를 사용하십시오."
+    elif device == "mouse":
+        texts["responseText"] = "느리게 하려면 마우스 왼쪽 버튼을 사용하고, 빠르게 하려면 마우스 오른쪽 버튼을 사용하십시오."
+
+    texts[
+        "Tutorial1"
+    ] = """실험 동안 당신의 맥박을 기록하고, 심박수에 따라 삐 소리를 재생합니다.
+
+당신은 심장 박동의 내부 감각에만 집중할 수 있으며, 손목이나 목에서 맥박을 확인하는 등의 다른 방법으로 심박수를 측정해서는 안됩니다.
+        """
+    texts[
+        "pulseTutorial1"
+    ] = "집게손가락에 센서를 착용하십시오. 평소 주로 사용하지 않는 손을 사용하십시오."
+
+    texts[
+        "pulseTutorial2"
+    ] = "집게손가락에 센서가 있을 때 심장 박동이 느껴지면, 다른 손가락에 착용하십시오.""
+
+    texts[
+        "pulseTutorial3"
+    ] = "심박수가 가장 적게 느껴지는 손가락을 찾을 때까지 다른 손가락에 착용해 시험하십시오."
+
+    texts[
+        "pulseTutorial4"
+    ] = "센서를 착용하기로 한 손가락에 해당하는 번호를 입력하십시오."
+
+    texts[
+        "Tutorial2"
+    ] = "이 아이콘이 보이면 5초 동안 심장 박동에 주의를 집중하십시오. 이 기간 동안 맥박을 기록하고 있으므로 움직이지 마십시오."
+
+    moreResp = "위 방향 키" if device == "keyboard" else "마우스 오른쪽 버튼"
+    lessResp = "아래 방향 키" if device == "keyboard" else "마우스 왼쪽 버튼"
+    texts[
+        "Tutorial3_icon"
+    ] = """심장 박동에 귀 기울이는 기간이 끝나면, 같은 아이콘이 표시되고 삐- 소리들을 듣게 됩니다."""
+    texts[
+        "Tutorial3_responses"
+    ] = f"""삐- 소리를 듣고 가능한 한 빠르고 정확하게, 삐 소리가 자신의 심박수보다 빠른지 ({moreResp}) 느린지 ({lessResp}) 결정합니다.
+
+삐 소리는 항상 심장 박동보다 느리거나 빠릅니다. 잘 모르겠어도 둘 중 하나를 선택하십시오."""
+
+    if exteroception is True:
+        texts[
+            "Tutorial3bis"
+        ] = """일부 시행에서는 하트 아이콘 대신 듣기 아이콘이 표시됩니다. 이 때는 당신의 심장이 아니라, 삐 소리들에 귀 기울여야 합니다."""
+
+        texts[
+            "Tutorial3ter"
+        ] = f"""삐 소리들이 들리고 난 후, 응답 아이콘이 나타나고 두 번째 삐 소리들을 듣게 됩니다.
+
+두 번째 삐 소리들을 들은 다음, 이 소리들이 첫 번째 삐 소리들보다 빠른지 ({moreResp}) 혹은 느린지 ({lessResp}) 가능한 한 빠르고 정확하게 응답하십시오.
+
+두 번째 삐 소리들은 항상 첫 번째 삐 소리들보다 느리거나 빠릅니다. 잘 모르겠어도 둘 중 하나를 선택하십시오."""
+
+    texts[
+        "Tutorial4"
+    ] = """응답하고 나면, 당신의 응답에 얼마나 자신이 있는지 평가하게 됩니다.
+
+여기서 최대 (100)은 선택에 대해 전적으로 확신한다는 것을 의미하고 가장 낮은 등급(0)은 추측하고 있다고 느꼈다는 것을 의미합니다.
+
+마우스를 이용해 응답해 주십시오."""
+
+    texts[
+        "Tutorial5"
+    ] = """이러한 순서는 시행 마다 반복됩니다.
+
+때때로 과제가 매우 어려울 수 있습니다. 실제 심박수와 표시된 경고음 간의 차이는 매우 작을 수 있습니다.
+
+따라서, 당신은 자신의 응답에 대한 확신을 평가할 때, 전체 척도 범위 (0에서 100까지)를 사용해야 합니다.
+
+과제 난이도는 시간이 지남에 따라 변하기 때문에 완전히 자신감이 있거나 완전히 불확실한 경우는 드뭅니다."""
+
+    texts[
+        "Tutorial6"
+    ] = """설명을 마치겠습니다. 궁금한 점이 있으면 지금 실험자에게 물어보십시오.
+그렇지 않으면, 본 시행을 시작할 수 있습니다."""
 
     return texts
